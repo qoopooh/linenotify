8a405193c2f1dc0ed2dbe4d52ee59504a5e96985
Berm S10 <berm@s10.termux>
2022-07-28 19:58:12 +0700

    Update readme

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # linenotify
 LINE Notify module
 
+## Sample code
+```go
+package main
+
+import (
+	"os"
+
+	"github.com/qoopooh/linenotify/notify"
+)
+
+func main() {
+	msg := "line_notify"
+
+	if len(os.Args) > 1 {
+		msg = os.Args[1]
+	}
+
+	param := notify.SendOpts{
+		Token:   os.Getenv("LINE_NOTIFY_TOKEN"),
+		Prefix:  "sample",
+		Message: msg,
+		Verbose: true,
+	}
+
+	notify.Send(param)
+}
+```
+
 ## Test
 Follow the instruction [here](https://go.dev/doc/modules/publishing)
 ```sh
--- notify/notify.go
+++ notify/notify.go
@@ -10,14 +10,16 @@ import (
 )
 
 type SendOpts struct {
-	Token   string
-	Message string
-	Prefix  string
-	Verbose bool // print out debug message
+	Token   string // LINE notify token
+	Message string // your message
+	Prefix  string // prefix of a message with [squre brackets]
+	Verbose bool   // print out debug message
 }
 
 func Send(opt SendOpts) string {
+
 	const URL = "https://notify-api.line.me/api/notify"
+
 	data := url.Values{}
 	if opt.Prefix != "" {
 		data.Set("message", fmt.Sprintf("[%s] %s", opt.Prefix, opt.Message))
@@ -29,13 +31,13 @@ func Send(opt SendOpts) string {
 		fmt.Printf("Send: %v\n", data)
 	}
 
-	client := &http.Client{}
 	req, _ := http.NewRequest(http.MethodPost,
 		URL,
 		strings.NewReader(data.Encode()))
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", opt.Token))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
+	client := &http.Client{}
 	resp, err := client.Do(req)
 
 	if err != nil {
@@ -46,15 +48,14 @@ func Send(opt SendOpts) string {
 	var res map[string]interface{}
 	json.NewDecoder(resp.Body).Decode(&res)
 
-	if opt.Verbose {
-		fmt.Printf("Resp: %v\n", res)
-	}
-
 	out := fmt.Sprintf("%v", res["message"])
 	if out == "Missing Bearer" {
 		out = fmt.Sprintf(
-			"%s (Please set LINE_NOTIFY_TOKEN)",
-			out)
+			"%s (Please set LINE_NOTIFY_TOKEN)", out)
+	}
+
+	if opt.Verbose {
+		fmt.Printf("Resp: %v\n", res)
 	}
 
 	return out
